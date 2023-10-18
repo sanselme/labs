558ee63b22819dbbd825626ea52f854eb2956959
Schubert Anselme <schubert@anselm.es>
2023-10-18 14:09:31 -0400

    Update vision demo site (#22)
    **Release notes**:
    
    <!--  Steps to write your release note:
    1. Use the release-note-* labels to set the release note state (if you
    have access)
    2. Enter your extended release note in the below block; leaving it blank
    means using the PR title as the release note. If no release note is
    required, just write `NONE`.
    -->
    
    ```release-note
    
    ```
    
    ---------
    
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- scripts/genconfig.sh
+++ scripts/genconfig.sh
@@ -17,7 +17,17 @@
 source scripts/load-env.sh
 
 : "${SITE:=$1}"
-[[ -z ${SITE} ]] && echo "Usage: $0 <site>" && exit 1
+: "${KUBECONFIG:=$2}"
+[[ -z ${SITE} ]] && {
+  echo """
+Usage: $0 <site> [kubeconfig]
+  site:       name of the site
+  kubeconfig: path to kubeconfig file (default: /tmp/kubeconfig.yaml)
+
+Example:
+  $0 vision
+"""
+}
 
 : "${BUILD_ROOT:=build/site/${SITE}}"
 : "${CLOUDINIT:=${BUILD_ROOT}/cloud-init.yaml}"
@@ -28,35 +38,17 @@ source scripts/load-env.sh
 : "${SETUP_MACHINE_SCRIPT=$(cat scripts/setup-machine.sh)}"
 : "${SSH_PUB_KEY:=${BUILD_ROOT}/id_rsa.pub}"
 
-# retrieve ssh key
-if [[ -f ${SSH_PUB_KEY} ]]; then
-  echo "SSH key already generated!!!"
-else
-  SSH_PUB_KEY_CONTENT=$(./scripts/onepassword/op-get-item.sh "${OP_SSH_KEY}" | jq '.fields[] | select(.id | contains("public_key")) | .value')
-  touch "${SSH_PUB_KEY}"
-  echo "${SSH_PUB_KEY_CONTENT}" | tr -d '"' >"${SSH_PUB_KEY}"
-fi
+# TODO: parametrize bootstrap-kubeconfig
+[[ -z ${KUBECONFIG} ]] && {
+  KUBECONFIG="/tmp/kubeconfig.yaml"
+  cat <<EOF >"${KUBECONFIG}"
+apiVersion: v1
+kind: Config
+preferences: {}
+EOF
+}
 
-SSH_PUB_KEY=$(cat "${SSH_PUB_KEY}")
-export SSH_PUB_KEY
-
-# retrieve keystore
-if [[ -f ${KEYSTORE} ]]; then
-  echo "Keystore already generated!!!"
-else
-  ./scripts/onepassword/op-get-file.sh "${KEYSTORE_NAME}" "${KEYSTORE}"
-fi
-
-# TODO: retrieve ca cert
-# KEYSTORE_SECRET="$(./scripts/onepassword/op-get-item.sh "${OP_CREDS_TITLE}" keystore | jq '.value')"
-# openssl pkcs12 \
-#   -in "${KEYSTORE}" \
-#   -nodes \
-#   -nokeys \
-#   -passin pass:"${KEYSTORE_SECRET}" |
-#   sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
-
-BOOTSTRAP_KUBECONFIG=""
+BOOTSTRAP_KUBECONFIG="$(cat "${KUBECONFIG}")"
 BYOH_HOSTAGENT_SERVICE="$(cat deployment/baremetal/byoh-hostagent.service)"
 BYOH_HOSTAGENT_WATCHER_PATH="$(cat deployment/baremetal/byoh-hostagent-watcher.path)"
 BYOH_HOSTAGENT_WATCHER_SERVICE="$(cat deployment/baremetal/byoh-hostagent-watcher.service)"
@@ -75,8 +67,98 @@ else
   echo "Generating cloud-init..."
 
   cp -f config/cloud-init.yaml "${CLOUDINIT}"
-
-  yq -i '.users[].ssh_authorized_keys[0] = env(SSH_PUB_KEY)' "${CLOUDINIT}"
-  # yq -i '.ca_certs.trusted[0] = load_str(env(CA_CERT_FILE))' "${CLOUDINIT}"
   yq -i '(.. | select(tag == "!!str")) |= envsubst(nu)' "${CLOUDINIT}"
+
+  # add if requesting ssh-key
+  [[ -n ${ENABLE_SSH_KEY} ]] && {
+    # retrieve ssh key
+    if [[ -f ${SSH_PUB_KEY} ]]; then
+      echo "SSH key already generated!!!"
+    else
+      SSH_PUB_KEY_CONTENT=$(./scripts/onepassword/op-get-item.sh "${OP_SSH_KEY}" | jq '.fields[] | select(.id | contains("public_key")) | .value')
+      touch "${SSH_PUB_KEY}"
+      echo "${SSH_PUB_KEY_CONTENT}" | tr -d '"' >"${SSH_PUB_KEY}"
+    fi
+
+    SSH_PUB_KEY=$(cat "${SSH_PUB_KEY}")
+    export SSH_PUB_KEY
+
+    yq -i '.users[].ssh_authorized_keys[0] = env(SSH_PUB_KEY)' "${CLOUDINIT}"
+  }
+
+  # add if requesting trusted ca
+  [[ -n ${ENABLE_TRUSTED_CA} ]] && {
+    # retrieve keystore
+    if [[ -f ${KEYSTORE} ]]; then
+      echo "Keystore already generated!!!"
+    else
+      ./scripts/onepassword/op-get-file.sh "${KEYSTORE_NAME}" "${KEYSTORE}"
+    fi
+
+    # FIXME: extract ca cert
+    KEYSTORE_SECRET="$(./scripts/onepassword/op-get-item.sh "${OP_CREDS_TITLE}" keystore | jq '.value')"
+    openssl pkcs12 \
+      -in "${KEYSTORE}" \
+      -nodes \
+      -nokeys \
+      -passin pass:"${KEYSTORE_SECRET}" |
+      sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
+
+    yq -i '.ca_certs.trusted[0] = load_str(env(CA_CERT_FILE))' "${CLOUDINIT}"
+  }
+
+  # TODO: add if requesting bootstrap-kubeconfig
+  [[ -n ${ENABLE_BOOTSTRAP_KUBECONFIG} ]] && {
+    BOOTSTRAP_KUBECONFIG_CA_CERT="$(yq '.clusters[0].cluster.certificate-authority-data' "${BOOTSTRAP_KUBECONFIG_FILE}" | base64 -d)"
+    export BOOTSTRAP_KUBECONFIG_CA_CERT
+
+    yq -i '.ca_certs.trusted[1] = "'"${BOOTSTRAP_KUBECONFIG_CA_CERT}"'"' "${CLOUDINIT}"
+  }
+
+  # TODO: add if requesting ceph-loopdev
+  [[ -n ${ENABLE_CEPH_LOOPDEV} ]] && {
+    extrafile_content="$(cat scripts/setup-ceph-loopdev.sh)"
+    extrafile="{path: /opt/setup-ceph-loopdev.sh, permissions: 755, owner: root, content: \n}"
+    export extrafile_content extrafile
+
+    yq -i '.write_files += env(extrafile)' "${CLOUDINIT}"
+    yq -i '.write_files[5].content = load_str("./scripts/setup-ceph-loopdev.sh")' "${CLOUDINIT}"
+    yq -i '.runcmd += "/opt/setup-ceph-loopdev.sh"' "${CLOUDINIT}"
+  }
+
+  # TODO: add if requesting install-docker
+  [[ -n ${INSTALL_DOCKER} ]] && {
+    extrafile_content="$(cat scripts/install-docker.sh)"
+    extrafile="{path: /opt/install-docker.sh, permissions: 755, owner: root, content: \n}"
+    export extrafile_content extrafile
+
+    yq -i '.write_files += env(extrafile)' "${CLOUDINIT}"
+    yq -i '.write_files[5].content = load_str("./scripts/install-docker.sh")' "${CLOUDINIT}"
+    yq -i '.runcmd += "/opt/install-docker.sh"' "${CLOUDINIT}"
+  }
+
+  # TODO: add if requesting install-cri-docker
+  [[ -n ${INSTALL_CRI_DOCKER} ]] && {
+    extrafile_content="$(cat scripts/install-cri-docker.sh)"
+    extrafile="{path: /opt/install-cri-docker.sh, permissions: 755, owner: root, content: \n}"
+    export extrafile_content extrafile
+
+    yq -i '.write_files += env(extrafile)' "${CLOUDINIT}"
+    yq -i '.write_files[5].content = load_str("./scripts/install-cri-docker.sh")' "${CLOUDINIT}"
+    yq -i '.runcmd += "/opt/install-cri-docker.sh"' "${CLOUDINIT}"
+  }
+
+  # TODO: add if requesting install-kubernetes
+  [[ -n ${INSTALL_KUBERNETES} ]] && {
+    extrafile_content="$(cat scripts/install-kubernetes.sh)"
+    extrafile="{path: /opt/install-kubernetes.sh, permissions: 755, owner: root, content: \n}"
+    export extrafile_content extrafile
+
+    yq -i '.write_files += env(extrafile)' "${CLOUDINIT}"
+    yq -i '.write_files[5].content = load_str("./scripts/install-kubernetes.sh")' "${CLOUDINIT}"
+    yq -i '.runcmd += "/opt/install-kubernetes.sh"' "${CLOUDINIT}"
+  }
 fi
+
+# encrypt
+./tools/sops/encrypt.sh "${CLOUDINIT}"

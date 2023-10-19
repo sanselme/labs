6193c8a5a9d2d1d6ecd981bb0c052dabe652ad2c
Schubert Anselme <schubert@anselm.es>
2023-10-19 12:58:36 -0400

    fix: Build scripts
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- tools/bottle/clos.sh
+++ tools/bottle/clos.sh
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-set -e
+# set -e
 
 export CLUSTERCTL_VERSION=1.5.2
 export COSIGN_VERSION=2.2.0
